8e702812af2888868d9a33a7b7d78beeeaa4c26d
sajib Hossen <sajibhossen560@gmail.com>
2026-07-29 21:15:53 +0600

    constants create

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // app logos
+  static const String darkAppLogo = "";
+  static const String lightAppLogo = "";
+
+  // social logos 
+  static const String google = '';
+  static const String facebook = '';
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,20 @@
+class TTexts {
+  // global texts
+
+  // onBoarding texts
+
+  static const String onBoardingTitle1 = 'Choose your product';
+  static const String onBoardingTitle2 = 'Select Payment Method';
+  static const String onBoardingTitle3 = 'Deliver at your door step';
+  static const String onBoardingSubTitle1 =
+      'Wellcome to a world of limitless Choices - Your prefect Product Awaits!';
+  static const String onBoardingSubTitle2 =
+      'For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!';
+  static const String onBoardingSubTitle3 =
+      'From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!';
+
+  // home
+
+  static const String homeAppBarTitle = 'Good day for shopping';
+  static const String homeAppBarSubTitle = 'Taimoor sikander';
+}
