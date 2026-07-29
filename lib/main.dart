ec66ff53e4086d179a139aeb5ea9b26ce2c56796
sajib Hossen <sajibhossen560@gmail.com>
2026-07-29 20:57:13 +0600

    sizes created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:c_commerce/app.dart';
+import 'package:c_commerce/utils/constants/colors.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
+  
   runApp(const App());
 }
--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -8,6 +8,8 @@ class TColors {
   static const Color secondary = Color(0xffffe24b);
   static const Color accent = Color(0xffb0c7ff);
 
+// gradient colors 
+
   // text colors
   static const Color textPrimary = Color(0xff333333);
   static const Color textSecondary = Color(0xff6c757d);
@@ -33,4 +35,20 @@ class TColors {
 
   static const Color borderPrimary = Color(0xffd9d9d9);
   static const Color bordersecondary = Color(0xffe6e6e6);
+
+  // Error and validation colors
+  static const Color error = Color(0xffd32f2f);
+  static const Color succes = Color(0xff388e3c);
+  static const Color warning = Color(0xfff57c00);
+  static const Color info = Color(0xff1976d2);
+
+  // neutral shades
+
+  static const Color black = Color(0xff232323);
+  static const Color darkerGrey = Color(0xff4f4f4f);
+  static const Color darkGrey = Color(0xff939393);
+  static const Color grey = Color(0xffe0e0e0);
+  static const Color softGrey = Color(0xfff4f4f4);
+  static const Color lightGrey = Color(0xfff9f9f9);
+  static const Color white = Color(0xffffffff);
 }
