79d9a866932748853847da7aa58d2af003ae7984
sajib Hossen <sajibhossen560@gmail.com>
2026-07-29 20:28:17 +0600

    colors setup

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TColors {
+  TColors._();
+
+  // app basic Colors
+  static const Color primary = Color(0xff4b68ff);
+  static const Color secondary = Color(0xffffe24b);
+  static const Color accent = Color(0xffb0c7ff);
+
+  // text colors
+  static const Color textPrimary = Color(0xff333333);
+  static const Color textSecondary = Color(0xff6c757d);
+  static const Color textWhite = Colors.white;
+
+  //  Background Colors
+  static const Color light = Color(0xfff6f6f6);
+  static const Color dark = Color(0xff272727);
+  static const Color primaryBackground = Color(0xfff3f5ff);
+
+  // background container colors
+
+  static const Color lightContainer = Color(0xfff6f6f6);
+  static final Color darkContainer = Colors.white.withOpacity(0.1);
+
+  // button colors
+
+  static const Color buttonPrimary = Color(0xff4b68ff);
+  static const Color buttonSecondary = Color(0xff6c757d);
+  static const Color buttonDisable = Color(0xffc4c4c4);
+
+  // border color
+
+  static const Color borderPrimary = Color(0xffd9d9d9);
+  static const Color bordersecondary = Color(0xffe6e6e6);
+}
